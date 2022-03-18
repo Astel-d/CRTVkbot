1d5489a08cd919c9ae57f47e8d7b6efd17d9f26d
Daniil <101560362+Astel-dev@users.noreply.github.com>
2022-03-18 19:09:17 +0300

    Add files via upload

---
+++ responses.py
@@ -0,0 +1,43 @@
+responses = {
+	'ru':{
+		'upload_file':"Отправьте файл для загрузки на коптер, учтите, файл должен иметь расширение '.py'.",
+		'dont_code': "Это не похоже на код, убедитесь, что вы отправили файл с расширением '.py'.",
+		'done':"Выполнено.",
+		'photo':'Последнее фото с клевераю.',
+		'return':["Возвращаюсь на координаты пользователя.","Возвращаюсь на координаты взлёта."],
+		'location':["Отправьте своё местоположение по инструкции ниже.", "Это не похоже на местоположение."],
+		'settings':['Отлично!Далее введите название аргумента и ЧЕРЕЗ ПРОБЕЛ его значение(Примеры ниже), учтите, что скорость не должна превышать 10(м/с), высота не должна превышать 100(м), действие может иметь значения "hover" и "land", а место возврата "user" и "takeoff".\n speed 9\n alt 3\n action hover\n to user'],
+		'auth':[
+				'Отлично, далее авторизуйтесь.',
+				'Введите логин.', 'Введите Пароль.',
+				'Такого пользователя не существует. Проверьте праильность логина или пройдите регистрацию на сайте, а потом повторите попытку. https://bit.ly/3tWpqCw',
+				'Неверный пароль, попробуйте ещё раз.',
+				'Успешно.'
+				],
+		'report':[
+				"Напишите сообщение, оно будет передано в техподдержку, постарайтесь описать суть проблемы как можно точнее, тогда мы быстрее сможем её решить. Если вы нажали кнопку случайно, то напишите: 'Стоп'.",
+				"Отправка сообщения приостановлена.",
+				"Сообщение передано, ожидайте ответа в течение 12 часов, если ответ не поступил, то повторите своё сообщение."
+				]
+	},
+	'en':{
+		'upload_file':"Send the file to be uploaded to the quadcopter, please note that the file must have a '.py' extension.",
+		'dont_code':"This doesn't look like code, make sure you submit a '.py' file.",
+		'done':"Done.",
+		'photo':'Last photo from clover.',
+		'return':["I return to the user's coordinates.","I return to the takeoff coordinates."],
+		'location':["Submit your location using the instructions below.", "It doesn't look like a geolocation."],
+		'auth':[
+				'Great, then log in.',
+				'Enter login.', 'Enter password.',
+				'Such a user does not exist. Check if your login is correct or register on the site, and then try again. https://bit.ly/3tWpqCw',
+				'Wrong password, please try again.',
+				'Successfully.'
+				],
+		'report':[
+				"Write a message, it will be transferred to technical support, try to describe the essence of the problem as accurately as possible, then we will be able to solve it faster. If you pressed the button by accident, then write: 'Stop'.",
+				"Sending a message has been suspended.",
+				"The message has been sent, expect a response within 12 hours, if there is no response, then repeat your message.",
+				]
+	}			
+}
