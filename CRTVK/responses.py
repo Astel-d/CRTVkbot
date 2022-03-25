c98b1f0d33c8d28054cf632f37dbbbdcb800b0c9
Daniil <101560362+Astel2022@users.noreply.github.com>
2022-03-26 01:34:33 +0300

    New responses

--- CRTVK/responses.py
+++ CRTVK/responses.py
@@ -2,42 +2,35 @@ responses = {
 	'ru':{
 		'upload_file':"Отправьте файл для загрузки на коптер, учтите, файл должен иметь расширение '.py'.",
 		'dont_code': "Это не похоже на код, убедитесь, что вы отправили файл с расширением '.py'.",
-		'done':"Выполнено.",
-		'photo':'Последнее фото с клевераю.',
-		'return':["Возвращаюсь на координаты пользователя.","Возвращаюсь на координаты взлёта."],
-		'location':["Отправьте своё местоположение по инструкции ниже.", "Это не похоже на местоположение."],
-		'settings':['Отлично!Далее введите название аргумента и ЧЕРЕЗ ПРОБЕЛ его значение(Примеры ниже), учтите, что скорость не должна превышать 10(м/с), высота не должна превышать 100(м), действие может иметь значения "hover" и "land", а место возврата "user" и "takeoff".\n speed 9\n alt 3\n action hover\n to user'],
+		'done':"Успешно.",
+		'stop':'Приостановлено.',
+		'photo':'Последнее фото с клевера.',
+		'return':["Возвращаюсь на координаты пользователя.","Возвращаюсь на координаты взлёта.","Не настроены параметры возврата, текущие параметры: "],
+		'location':"Отправьте местоположение.",
+		'settings':['Отлично!Далее введите название аргумента и ЧЕРЕЗ ПРОБЕЛ его значение(Примеры ниже), учтите, что скорость не должна превышать 10(м/с), высота не должна превышать 100(м), действие может иметь значения "hover" и "land", а место возврата "user" и "takeoff".\nПримеры: \n speed 1\n alt 1\n action hover\n to user',"Текущие настройки: \n", "Недопустимое значение параметра."],
 		'auth':[
 				'Отлично, далее авторизуйтесь.',
-				'!!!Отправьте сначала логин, а потом пароль!!!',
-				'Такого пользователя не существует. Проверьте праильность логина или пройдите регистрацию на сайте, а потом повторите попытку. https://bit.ly/3tWpqCw',
+				'!!!Сначала вводите логин, а потом пароль!!!',
+				'Такого пользователя не существует. Проверьте правильность логина или пройдите регистрацию на сайте, а потом повторите попытку. https://bit.ly/3tWpqCw',
 				'Неверный пароль, попробуйте ещё раз.',
 				'Успешно.'
-				],
-		'report':[
-				"Напишите сообщение, оно будет передано в техподдержку, постарайтесь описать суть проблемы как можно точнее, тогда мы быстрее сможем её решить. Если вы нажали кнопку случайно, то напишите: 'Стоп'.",
-				"Отправка сообщения приостановлена.",
-				"Сообщение передано, ожидайте ответа в течение 12 часов, если ответ не поступил, то повторите своё сообщение."
-				]
-	},
+				]},
+
 	'en':{
 		'upload_file':"Send the file to be uploaded to the quadcopter, please note that the file must have a '.py' extension.",
 		'dont_code':"This doesn't look like code, make sure you submit a '.py' file.",
-		'done':"Done.",
+		'done':"Successfully.",
+		'stop':"Suspended.",
 		'photo':'Last photo from clover.',
-		'return':["I return to the user's coordinates.","I return to the takeoff coordinates."],
-		'location':["Submit your location using the instructions below.", "It doesn't look like a geolocation."],
+		'return':["I return to the user's coordinates.","I return to the takeoff coordinates.", "Return parameters not configured, current parameters: "],
+		'location':["Send location."],
+		'settings':['Excellent! Next, enter the name of the argument and its value SEPARATELY with a SPACE (Examples below), note that the speed should not exceed 10 (m / s), the height should not exceed 100 (m), the action can have the values of "hover" and "land ", and the return location is "user" and "takeoff".\nExamples: \n speed 1\n alt 1\n action hover\n to user',"Current settings: \n", "Invalid parameter value."],
 		'auth':[
 				'Great, then log in.',
-				'!!!Send your login first, and then the password!!!',
+				'!!!First, enter your login, and then the password!!!',
 				'Such a user does not exist. Check if your login is correct or register on the site, and then try again. https://bit.ly/3tWpqCw',
 				'Wrong password, please try again.',
 				'Successfully.'
-				],
-		'report':[
-				"Write a message, it will be transferred to technical support, try to describe the essence of the problem as accurately as possible, then we will be able to solve it faster. If you pressed the button by accident, then write: 'Stop'.",
-				"Sending a message has been suspended.",
-				"The message has been sent, expect a response within 12 hours, if there is no response, then repeat your message.",
 				]
 	}			
 }
