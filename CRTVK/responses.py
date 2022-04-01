ff9f8973f6c5d7229643899cab10686620fc4aac
Daniil <101560362+Astel2022@users.noreply.github.com>
2022-04-01 12:18:20 +0300

    Responses update
    Successfully → Succesfull.

--- CRTVK/responses.py
+++ CRTVK/responses.py
@@ -19,18 +19,18 @@ responses = {
 	'en':{
 		'upload_file':"Send the file to be uploaded to the quadcopter, please note that the file must have a '.py' extension.",
 		'dont_code':"This doesn't look like code, make sure you submit a '.py' file.",
-		'done':"Successfully.",
+		'done':"Successfull.",
 		'stop':"Suspended.",
 		'photo':'Last photo from clover.',
 		'return':["I return to the user's coordinates.","I return to the takeoff coordinates.", "Return parameters not configured, current parameters: "],
 		'location':["Send location."],
-		'settings':['Excellent! Next, enter the name of the argument and its value SEPARATELY with a SPACE (Examples below), note that the speed should not exceed 10 (m / s), the height should not exceed 100 (m), the action can have the values of "hover" and "land ", and the return location is "user" and "takeoff".\nExamples: \n speed 1\n alt 1\n action hover\n to user',"Current settings: \n", "Invalid parameter value."],
+		'settings':['Excellent! Next, enter the name of the argument and its value SEPARATELY with a SPACE (Examples below), note that the speed should not exceed 10 (m / s), the alt should not exceed 100 (m), the action can have the values of "hover" and "land ", and the return location is "user" and "takeoff".\nExamples: \n speed 1\n alt 1\n action hover\n to user',"Current settings: \n", "Invalid parameter value."],
 		'auth':[
 				'Great, then log in.',
 				'!!!First, enter your login, and then the password!!!',
 				'Such a user does not exist. Check if your login is correct or register on the site, and then try again. https://bit.ly/3tWpqCw',
 				'Wrong password, please try again.',
-				'Successfully.'
+				'Successfull.'
 				]
 	}			
 }
